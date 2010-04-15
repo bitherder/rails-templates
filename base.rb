c07ade66b6fe216a4c3b53f459b46af4a96de0c1
Larry Baltz <larry@baltz.org>
2010-04-15 14:17:04 +0200

    Add rr gem spec Make rr the mocking framework for spec

--- base.rb
+++ base.rb
@@ -1,3 +1,20 @@
+def spec_config_add(data = nil, options = {}, &block)
+  sentinel = 'Spec::Runner.configure do |config|'
+
+  data = block.call if !data && block_given?
+
+  in_root do
+    gsub_file 'spec/spec_helper.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+      "#{match}\n  " << data
+    end
+  end
+end
+
+def spec_config(name, value = '')
+  log 'spec_config', name
+  spec_config_add "config.#{name} = #{value.inspect}"
+end
+
 git :init
 
 file 'tmp/.gitignore', <<-END
@@ -25,13 +42,14 @@ END
 git :add => '.', :commit => '-m "just generated from rails command"'
 
 run "echo 'TODO add readme content' > README"
-git :add => ".", :commit => "-m 'clear out README'"
+git :add => '.', :commit => '-m "clear out README"'
 
-gem 'cucumber',                     :environment => :test
-gem 'faker',                        :environment => :test
 gem 'rspec',        :lib => 'spec', :environment => :test
 gem 'rspec-rails',  :lib => false,  :environment => :test
+gem 'faker',                        :environment => :test
 gem 'machinist',                    :environment => :test
+gem 'rr',                           :environment => :test
+gem 'cucumber',                     :environment => :test
 gem 'pickle',                       :environment => :test
 
 gem 'rdiscount'
@@ -45,6 +63,8 @@ rake 'gems:install', :env => :test, :sudo => true
 generate :rspec
 generate :cucumber, '--capybara'
 generate :pickle, :paths, :emails
+
+spec_config :mock_with, :mocha
 
 git :add => '.'
 git :commit => '-m "test scaffolding generated"'
